1a497e8e0516d6df4ef8d266595866defd254a2a
LokiObahoshe <sondereggerbecca@gmail.com>
2024-09-25 01:08:41 -0500

    W02 Prepare: Learning Activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2020;
+        job1._endYear = 2030;
+
+        Job job2 = new Job();
+
+        job2._jobTitle = "Employee";
+        job2._company = "Facebook";
+        job2._startYear = 2000;
+        job2._endYear = 2010;
+
+        Resume resume = new Resume();
+        resume._name = "Michael Afton";
+
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.DisplayResumeDetails();
     }
+    
 }
