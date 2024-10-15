c0125196361373a42f8246475b5801aab7b317ea
LokiObahoshe <sondereggerbecca@gmail.com>
2024-10-15 15:49:44 -0500

    W05 Prepare: Learning Activity #1

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Assignment assignment = new Assignment("William Afton", "Multiplication");
+        Console.WriteLine(assignment.GetSummary());
+
+        MathAssignment mathassignment = new MathAssignment("Ash Ketchem", "Substraction", "7.3", "8-19");
+        Console.WriteLine();
+        Console.WriteLine(mathassignment.GetSummary());
+        Console.WriteLine(mathassignment.GetHomeworkList());
+
+        WritingAssignment writingassignment = new WritingAssignment("Mario Mario", "English", "To Kill a Mockingbird");
+        Console.WriteLine();
+        Console.WriteLine(writingassignment.GetSummary());
+        Console.WriteLine(writingassignment.GetWritingInformation());
     }
 }
