b4fb3efdd4f324fda951e5d94440fce850c9645f
LokiObahoshe <sondereggerbecca@gmail.com>
2024-10-22 23:13:17 -0500

    W06 Prepare: Learning Activity

---
+++ prepare/Learning06/Circle.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class Circle : Shape
+{
+    private double _radius;
+
+    public Circle(string color, double radius) : base (color)
+    {
+        _radius = radius;
+    }
+
+    public override double GetArea()
+    {
+        return _radius * _radius * Math.PI;
+    }
+
+    public override string GetShapeType()
+    {
+        return "circle";
+    }
+}
--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        List<Shape> _shapes = new List<Shape>();
+
+        Square square = new Square("blue", 5);
+        _shapes.Add(square);
+
+        Rectangle rectangle = new Rectangle("purple", 8, 9);
+        _shapes.Add(rectangle);
+
+        Circle circle = new Circle("yellow", 2);
+        _shapes.Add(circle);
+
+        Console.Clear();
+        foreach (Shape shapes in _shapes)
+        {
+            string color = shapes.GetColor();
+            double area = shapes.GetArea();
+            string shapeType = shapes.GetShapeType();
+
+            Console.WriteLine($"The {color} {shapeType} has an area of {area}");
+        }
+
     }
 }
